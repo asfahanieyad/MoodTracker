bf5ef0c7199644456629bf369d8d01a5ba7b124c
eyad <asfahanieyad@gmail.com>
2025-02-19 14:42:21 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import javax.swing.*;
+
 public class Main {
+    private JPanel panel1;
+    private JPanel basePanel;
+    private JPanel logPanel;
+    private JPanel historyPanel;
+    private JPanel settingsPanel;
+    private JButton settingsButton;
+    private JButton moodHistoryButton;
+    private JButton exitButton;
+    private JButton logMoodButton;
+    private JLabel label1;
+
+    public Main(){
 
 
+    }
 
-    //this is main
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Mood Tracker");
+        frame.setContentPane(new Main().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(650, 600);
+        frame.setLocation(400, 200);
+        frame.setVisible(true);
+
+    }
 }
