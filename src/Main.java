3dcd355b8c624d37e29885c4b85b301de44625e1
eyad <asfahanieyad@gmail.com>
2025-02-19 14:52:19 -0500

    update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
     private JPanel panel1;
@@ -11,9 +13,23 @@ public class Main {
     private JButton exitButton;
     private JButton logMoodButton;
     private JLabel label1;
+    private JComboBox comboBox1;
+    private JTextArea textArea1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton a1Button;
 
     public Main(){
 
+
+        logMoodButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                basePanel.setVisible(false);
+                logPanel.setVisible(true);
+            }
+        });
 
     }
 
